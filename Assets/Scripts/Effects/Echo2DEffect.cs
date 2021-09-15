9c0c889fde1590aa12f482fc1b22ad7a5cb698a3
msalo37 <39465475+msalo37@users.noreply.github.com>
2021-09-16 00:31:40 +0400

    Update Echo2DEffect.cs

--- Assets/Scripts/Effects/Echo2DEffect.cs
+++ Assets/Scripts/Effects/Echo2DEffect.cs
@@ -12,13 +12,17 @@ public class Echo2DEffect : MonoBehaviour
     private void Start()
     {
         StartCoroutine(EchoEffect());
+        color = GetComponent<SpriteRenderer>().color;
     }
 
+    private Color color;
+    
     private IEnumerator EchoEffect()
     {
         while(true)
         {
             Transform tr = Instantiate(prefab, transform.position, transform.rotation).transform;
+            tr.GetComponent<SpriteRenderer>().color = color;
             tr.DOScale(new Vector3(0, 0, 0), destroyTime).OnComplete(delegate { Destroy(tr.gameObject); });
             yield return new WaitForSeconds(spawnTime);
         }
